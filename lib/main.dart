5b90c0e6ad489221f00fbcd4aa5724c460262099
liangrongjun <liangrongjun@saicmobility.com>
2020-07-25 22:22:48 +0800

    外部框架

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,138 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main(List<String> args) {
+  return runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  int _selectedIndex = 0;
+  final List<Map<String, void>> tabsList =  [
+    {"key": 1, "label": '关注'},
+    {"key": 2, "label": '软件'},
+    {"key": 3, "label": '资讯'},
+    {"key": 4, "label": '推荐'},
+    {"key": 5, "label": '问答'},
+    {"key": 6, "label": '博客'},
+    {"key": 7, "label": '英文'}
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      '关注',
+      style: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold
+      )
+    ),
+    Text(
+      '软件',
+      style: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold
+      )
+    ),
+    Text(
+      '资讯',
+      style: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold
+      )
+    ),
+    Text(
+      '推荐',
+      style: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold
+      )
+    ),
+    Text(
+      '问答',
+      style: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold
+      )
+    ),
+    Text(
+      '博客',
+      style: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold
+      )
+    ),
+    Text(
+      '英文',
+      style: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold
+      )
+    )
+  ];
+  
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter22'),
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 7, 
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              isScrollable: true,
+              tabs: [
+                Tab(text: '关注'),
+                Tab(text: '软件'),
+                Tab(text: '资讯'),
+                Tab(text: '推荐'),
+                Tab(text: '问答'),
+                Tab(text: '博客'),
+                Tab(text: '英文'),
+              ]
+            )
+          ),
+          body: _widgetOptions.elementAt(_selectedIndex),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                title: Text('综合'),
+                icon: Icon(Icons.home)
+              ),
+              BottomNavigationBarItem(
+                title: Text('动弹'),
+                icon: Icon(Icons.business)
+              ),
+              BottomNavigationBarItem(
+                title: Text('+'),
+                icon: Icon(Icons.school)
+              ),
+              BottomNavigationBarItem(
+                title: Text('发现'),
+                icon: Icon(Icons.school)
+              ),
+              BottomNavigationBarItem(
+                title: Text('我的'),
+                icon: Icon(Icons.school)
+              ),
+            ],
+            onTap: _onItemTapped,
+            // backgroundColor: Color(0xff63ca6c),
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.amber[800],
+            type: BottomNavigationBarType.fixed,
+          ),
         ),
-        body: new Center(
-          child: new Text('123'),
-        ),
-      ),
+      )
     );
   }
 }
---
+++ lib/utils/constant.dart
@@ -0,0 +1,12 @@
+// app信息
+abstract class AppInfo {
+  static const tabsList = [
+    {"key": 1, "label": '关注'},
+    {"key": 2, "label": '软件'},
+    {"key": 3, "label": '资讯'},
+    {"key": 4, "label": '推荐'},
+    {"key": 5, "label": '问答'},
+    {"key": 6, "label": '博客'},
+    {"key": 7, "label": '英文'}
+  ];
+}
